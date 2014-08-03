0b03d19af5985a802a1d38eecf4baafc57599b91
Alex Kozlov <aleksei.e.kozlov@gmail.com>
2014-08-03 16:56:15 +0400

    первый залив

---
+++ Slot_LoadTesting/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Slot_LoadTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AccessTokenlabel = new System.Windows.Forms.Label();
+            this.AccessTokenTextBox = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.logRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AccessTokenlabel
+            // 
+            this.AccessTokenlabel.AutoSize = true;
+            this.AccessTokenlabel.Location = new System.Drawing.Point(12, 9);
+            this.AccessTokenlabel.Name = "AccessTokenlabel";
+            this.AccessTokenlabel.Size = new System.Drawing.Size(72, 13);
+            this.AccessTokenlabel.TabIndex = 6;
+            this.AccessTokenlabel.Text = "Access token";
+            // 
+            // AccessTokenTextBox
+            // 
+            this.AccessTokenTextBox.Location = new System.Drawing.Point(90, 6);
+            this.AccessTokenTextBox.Name = "AccessTokenTextBox";
+            this.AccessTokenTextBox.ReadOnly = true;
+            this.AccessTokenTextBox.Size = new System.Drawing.Size(170, 20);
+            this.AccessTokenTextBox.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.logRichTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.AccessTokenlabel);
+            this.splitContainer1.Panel1.Controls.Add(this.AccessTokenTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(732, 361);
+            this.splitContainer1.SplitterDistance = 278;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(450, 361);
+            this.webBrowser.TabIndex = 5;
+            // 
+            // logRichTextBox
+            // 
+            this.logRichTextBox.Location = new System.Drawing.Point(12, 32);
+            this.logRichTextBox.Name = "logRichTextBox";
+            this.logRichTextBox.ReadOnly = true;
+            this.logRichTextBox.Size = new System.Drawing.Size(248, 317);
+            this.logRichTextBox.TabIndex = 7;
+            this.logRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 361);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AccessTokenlabel;
+        private System.Windows.Forms.TextBox AccessTokenTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.RichTextBox logRichTextBox;
+
+    }
+}
+
---
+++ Slot_LoadTesting/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Slot_LoadTesting
+{
+    public class Logger : IDisposable
+    {
+        private readonly StreamWriter _streamWriter;
+        private readonly RichTextBox _richTextBox;
+
+        public Logger(string logFileName, RichTextBox richTextBox = null)
+        {
+            _streamWriter = new StreamWriter(logFileName, true);
+            if (richTextBox != null)
+                _richTextBox = richTextBox;
+            else
+                _richTextBox = new RichTextBox();
+        }
+
+
+        public void Dispose()
+        {
+            _streamWriter.Close();
+            _streamWriter.Dispose();
+        }
+
+        public void WriteLine(string message)
+        {
+            _streamWriter.WriteLine(message);
+            _richTextBox.Text += message + Environment.NewLine;
+        }
+    }
+}
